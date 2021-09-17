1b14a120cfdd7736b9104e9f8f94fe52182b36a4
Soul-Remix <soulremix-webdev@pm.me>
2021-09-17 09:39:08 +0300

    add mongoose and create schema

---
+++ models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  inStock: { type: Number, required: true, min: 0 },
+  image: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+});
+
+ProductSchema.virtual('url').get(() => {
+  return `/catalog/product/${this._id}`;
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
